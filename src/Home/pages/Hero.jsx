b70829b6a8ae85cc5cc1d225a236cf03d3e0f971
Saumya Modi <saumyamodi249@gmail.com>
2026-08-24 17:01:58 +0530

    hero fina; checked

--- src/Home/pages/Hero.jsx
+++ src/Home/pages/Hero.jsx
@@ -1,3 +1,5 @@
+import { PiPlusThin } from "react-icons/pi";
+
 export default function Hero() {
   return (
     <section
@@ -12,21 +14,22 @@ export default function Hero() {
       {/* Content */}
       <div className="relative z-10 flex h-full w-full items-start">
         <div className="w-full max-w-[1400px] px-6 md:px-10 lg:px-12 pt-24 lg:pt-28 translate-x-[20px]">
+
           {/* Heading */}
           <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl !font-semibold !mb-8 max-w-full md:max-w-xl lg:max-w-2xl lh-70 text-left">
             Smarter Interiors Start Here
           </h1>
+
           <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl !font-semibold !mb-8 max-w-full md:max-w-xl lg:max-w-2xl lh-70 text-left">
             — Welcome to <span className="text-[#D97967]">Desinary</span>
           </h1>
 
           {/* Description */}
-          <p className="text-xs lg:text-sm max-w-[100%] md:max-w-lg lg:max-w-md text-left !text-[var(--theme-banner-description-color)] !mb-8">
-            <p className="text-[#656565]">
+          <p className="text-xs lg:text-sm max-w-[100%] md:max-w-lg lg:max-w-md text-left !text-[#656565] !mb-8">
             Design your space the way you want with the power of AI and the
             freedom to personalize. Fast, intuitive, and beautifully intelligent
             - this is interior design, reimagined for you.
-          </p></p>
+          </p>
 
           {/* Start Designing Button */}
           <div className="mt-4 lg:mt-8">
@@ -35,15 +38,69 @@ export default function Hero() {
               className="text-sm lg:text-base rounded-md bg-[var(--theme-btn-bg)] px-[10px] sm:px-[14px] py-[8px] lg:py-[10px] !text-[var(--theme-bg-primary)] tracking-wide transition-all duration-300 ease-in-out hover:bg-[var(--theme-btn-bg-hover)] flex gap-2 items-center"
             >
               Start Designing
+
               <img
-    src="/Home/1st/arrow.svg"
-    alt="Arrow"
-    className="h-3.5 w-3.5 translate-y-[1px]"
-  />
+                src="/Home/1st/arrow.svg"
+                alt="Arrow"
+                className="h-3.5 w-3.5 translate-y-[1px]"
+              />
             </button>
           </div>
-        </div>
-      </div>
+
+          {/* Clients */}
+        <div className="mt-12 lg:mt-16">
+  {/* Profile Images */}
+  <div className="flex items-center">
+
+    <div className="relative z-10">
+      <img
+        src="/Home/1st/4th person.svg"
+        alt="Client"
+        className="w-[58px] h-[58px] sm:w-[64px] sm:h-[64px] rounded-full object-cover border-[3px] border-[#D97967]"
+      />
+    </div>
+
+    <div className="relative z-20 -ml-4">
+      <img
+        src="/Home/1st/3rd person.svg"
+        alt="Client"
+        className="w-[58px] h-[58px] sm:w-[64px] sm:h-[64px] rounded-full object-cover border-[3px] border-[#D97967]"
+      />
+    </div>
+
+    <div className="relative z-30 -ml-4">
+      <img
+        src="/Home/1st/2nd person.svg"
+        alt="Client"
+        className="w-[58px] h-[58px] sm:w-[64px] sm:h-[64px] rounded-full object-cover border-[3px] border-[#D97967]"
+      />
+    </div>
+
+    <div className="relative z-40 -ml-4">
+      <img
+        src="/Home/1st/1st person.svg"
+        alt="Client"
+        className="w-[58px] h-[58px] sm:w-[64px] sm:h-[64px] rounded-full object-cover border-[3px] border-[#D97967]"
+      />
+    </div>
+
+  </div>
+
+  {/* Client Count */}
+  <div className="flex items-center mt-4">
+  <span className="flex items-center text-center lg:text-left text-sm sm:text-base md:text-xl lg:text-3xl font-semibold text-[var(--theme-black)] whitespace-nowrap">
+    200<PiPlusThin className="inline-block w-4 h-4 lg:w-6 lg:h-6" />
+  </span>
+
+  <span className="ml-1 text-sm sm:text-base md:text-xl lg:text-3xl font-semibold text-[#D97967] whitespace-nowrap">
+    Clients
+  </span>
+</div>
+  </div>
+</div>
+</div>   
+
+      
     </section>
   );
 }
