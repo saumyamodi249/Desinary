0c80f8cde836ccb119b02fe976df7c810821ade6
Saumya Modi <saumyamodi249@gmail.com>
2026-08-31 16:01:53 +0530

    lines

--- src/AboutUs/flow/AboutUs.jsx
+++ src/AboutUs/flow/AboutUs.jsx
@@ -1,9 +1,18 @@
-function AboutUs() {
+import React from "react";
+import Hero from "../pages/Hero";
+import VisionMissionStory from "../pages/VisionMissionStory";
+import InvestmentBanner from "../pages/InvestmentBanner";
+import Team from "../pages/Team";
+
+const AboutUs = () => {
   return (
-    <div>
-      <h1>About Us</h1>
-    </div>
+    <>
+      <Hero />
+      <VisionMissionStory />
+      <InvestmentBanner />
+      <Team />
+    </>
   );
-}
+};
 
 export default AboutUs;
