35241cbe542ce716a022429be6f0392ed16e6f2c
Saumya Modi <saumyamodi249@gmail.com>
2026-09-10 14:32:38 +0530

    wig

--- src/Home/pages/MeetDesinary.jsx
+++ src/Home/pages/MeetDesinary.jsx
@@ -81,7 +81,6 @@ export default function MeetDesinary() {
       <div className="px-[120px] pt-[108px] pb-[60px] max-md:px-4 max-md:pt-12 max-md:pb-6">
         {/* Top Content */}
         <div className="mx-auto grid max-w-[1140px] grid-cols-1 md:grid-cols-[1.05fr_0.95fr] md:gap-14">
-
           {/* Heading */}
           <div className="flex w-full max-w-[600px] items-start max-md:items-center max-md:justify-center max-md:mx-auto">
             <h2 className="w-full max-w-[750px] -translate-x-16 -translate-y-4 text-[48px] font-semibold text-[var(--theme-title-text)] max-md:translate-x-0 max-md:translate-y-0 max-md:text-3xl max-md:text-center max-md:mb-6">
@@ -112,48 +111,35 @@ export default function MeetDesinary() {
       {/* Stats */}
       <div className="mx-auto w-full max-w-[1270px] bg-[var(--theme-bg-MeetDesinary-description)] max-md:bg-transparent max-md:px-4">
         <div className="w-full rounded-sm border border-[var(--theme-bg-MeetDesinary-border)] bg-[var(--theme-bg-MeetDesinary-box)] py-[16px] pl-[89px] pr-[90px] shadow-sm max-md:px-3 max-md:py-3 max-md:rounded-xl">
-
           {/* Inner Container */}
           <div className="relative mx-auto flex max-w-[1140px] items-center justify-between max-md:grid max-md:grid-cols-2 max-md:gap-0">
             {/* Small screen '+' cross divider lines */}
             <div className="hidden max-md:block absolute top-2 bottom-2 left-1/2 w-[1px] -translate-x-1/2 bg-[var(--theme-divider-color)] opacity-70 pointer-events-none" />
             <div className="hidden max-md:block absolute left-2 right-2 top-1/2 h-[1px] -translate-y-1/2 bg-[var(--theme-divider-color)] opacity-70 pointer-events-none" />
 
-            {meetDesinary.stats.map((s, index) => {
-              // On mobile, match screenshot: 125k+ (top-left), 50k+ (top-right), 12+ (bottom-left), 30+ (bottom-right)
-              const mobileOrder =
-                index === 1
-                  ? "max-md:order-1"
-                  : index === 0
-                  ? "max-md:order-2"
-                  : index === 3
-                  ? "max-md:order-3"
-                  : "max-md:order-4";
+            {meetDesinary.stats.map((s, index) => (
+              <div
+                key={s.label}
+                className="flex flex-1 items-center justify-between max-md:py-4 max-md:px-2"
+              >
+                {/* Stat Content */}
+                <div className="flex flex-1 flex-col items-center justify-center text-center">
+                  {/* Animated Number */}
 
-              return (
-                <div
-                  key={s.label}
-                  className={`flex flex-1 items-center justify-between max-md:py-5 max-md:px-2 ${mobileOrder}`}
-                >
-                  {/* Stat Content */}
-                  <div className="flex flex-1 flex-col items-center justify-center text-center">
+                  <AnimatedStat value={s.value} />
 
-                    {/* Animated Number */}
-                    <AnimatedStat value={s.value} />
-
-                    {/* Label */}
-                    <p className="mt-1 text-center font-[Poppins] text-[14px] font-normal text-[var(--theme-bg-MeetDesinary-box-text)]">
-                      {s.label}
-                    </p>
-                  </div>
-
-                  {/* Vertical Divider */}
-                  {index !== meetDesinary.stats.length - 1 && (
-                    <div className="hidden h-14 w-[1px] bg-[var(--theme-divider-color)] opacity-70 sm:block max-md:hidden" />
-                  )}
+                  {/* Label */}
+                  <p className="mt-1 text-center font-[Poppins] text-[14px] font-normal text-[var(--theme-bg-MeetDesinary-box-text)]">
+                    {s.label}
+                  </p>
                 </div>
-              );
-            })}
+
+                {/* Vertical Divider */}
+                {index !== meetDesinary.stats.length - 1 && (
+                  <div className="hidden h-14 w-[1px] bg-[var(--theme-divider-color)] opacity-70 sm:block max-md:hidden" />
+                )}
+              </div>
+            ))}
           </div>
         </div>
       </div>
